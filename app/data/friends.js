9dfc1d782af69c38317949437f23d93056a7a037
unknown <gustavogonz3393@gmail.com>
2018-09-15 00:02:05 -0500

    adding html

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendsArray = [{
+    "name": "Ahmed",
+    "photo": "https://www.pexels.com/photo/scenic-view-of-city-during-evening-1402778/",
+    "scores": [
+
+    ]
+}]
+
+
+module.exports = friendsArray;
